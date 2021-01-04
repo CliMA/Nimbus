011108fa1416ec75266a39f4b1f3b9bba5c34fbd
rose-water <rachel.ulgado@gmail.com>
2021-01-04 09:57:30 -0800

    resolving warning issues

--- src/AltVariableScrubber.js
+++ src/AltVariableScrubber.js
@@ -63,11 +63,11 @@ function AltVariableScrubber({
   // draws the handles for the time range
   // --------------------------------------------------------
   const handlePath = (d) => {
-    var e = +(d.type == "e"),
+    var e = +(d.type === "e"),
         x = e ? 1 : -1,
         y = time_size.h + margin.top;
     return "M" + (.5 * x) + "," + y + "A6,6 0 0 " + e + " " + (6.5 * x) + "," + (y + 6) + "V" + (2 * y - 6) +
-      "A6,6 0 0 " + e + " " + (.5 * x) + "," + (2 * y) + "Z" + "M" + (2.5 * x) + "," + (y + 8) + "V" + (2 * y - 8) +
+      "A6,6 0 0 " + e + " " + (.5 * x) + "," + (2 * y) + "ZM" + (2.5 * x) + "," + (y + 8) + "V" + (2 * y - 8) +
       "M" + (4.5 * x) + "," + (y + 8) + "V" + (2 * y - 8);
     }
 
