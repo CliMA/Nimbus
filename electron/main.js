aff64cdada7a41e9038cc14b65c37ac981edaafa
rose-water <rachel.ulgado@gmail.com>
2021-01-04 09:18:33 -0800

    minor changes for electron

--- electron/main.js
+++ electron/main.js
@@ -47,8 +47,8 @@ function createWindow () {
     slashes: true,
   });
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
